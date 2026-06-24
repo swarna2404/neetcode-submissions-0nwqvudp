8b88999bdd78774e25e7b3963e0f9028c6fdadc3
swarna2404 <swarnanachiyar13@gmail.com>
2026-06-24 17:43:56 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int farthest=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(i>farthest)return false;
+        
+        farthest=Math.max(farthest,i+nums[i]);}
+        if(farthest>=nums.length-1)return true;
+        return false;
+    }
+    
+}
